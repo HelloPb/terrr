302ae1e43ee36125a6b458cf50e62a4acc86fe7d
hellopb <prasanna.k.bhat@gmail.com>
2023-05-07 15:47:40 +1000

    k

--- examples/eks_managed_node_group/main.tf
+++ examples/eks_managed_node_group/main.tf
@@ -6,20 +6,20 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.default.token
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   # This requires the awscli to be installed locally where Terraform is executed
-  #   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  # }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 data "aws_eks_cluster" "default" {
-  name = local.cluster_name
+  name = local.name
 }
 
 data "aws_eks_cluster_auth" "default" {
-  name = local.cluster_name
+  name = local.name
 }
 
 data "aws_caller_identity" "current" {}
